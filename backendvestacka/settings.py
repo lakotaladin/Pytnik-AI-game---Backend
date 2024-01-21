f916b6a2100449e6e16e1a412ee4763be24e4aaa
aladin99 <aladin.dunp@gmail.com>
2024-01-21 21:07:36 +0100

    Deploy

--- backendvestacka/settings.py
+++ backendvestacka/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-svc^wjlpiwbcs$*jj#yl@)v2vujoq9o(qg0a#lnvq)3azairho
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*','.vercel.app']
 
 
 # Application definition
